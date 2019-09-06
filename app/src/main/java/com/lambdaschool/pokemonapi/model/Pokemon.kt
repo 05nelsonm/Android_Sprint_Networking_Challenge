1e695a59304dff924c197c9cd9c628fe6f19f33f
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-06 11:55:37 -0400

    created model for data

---
+++ app/src/main/java/com/lambdaschool/pokemonapi/model/Pokemon.kt
@@ -0,0 +1,9 @@
+package com.lambdaschool.pokemonapi.model
+
+class Pokemon (
+    val name: String,
+    val sprites: List<String>,
+    val id: String,
+    val abilities: List<String>,
+    val types: List<String>
+)
